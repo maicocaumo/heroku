743f0e228ee3cb2061cb40191894defcb75ec81c
Maico Rodrigo Caumo <maico.caumo@gmail.com>
2021-06-16 15:09:09 -0300

    Initial commit

---
+++ src/main/java/com/heroku/heroku/TestController.java
@@ -0,0 +1,13 @@
+package com.heroku.heroku;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String helloWord() {
+        return "Hello Word";
+    }
+}
